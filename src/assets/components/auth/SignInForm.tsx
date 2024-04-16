74059c524d040ea55695de22db18ec4b4a874060
Arthur <goolebyaqa@gmail.com>
2024-04-16 20:14:29 +0500

    feat: add responsive for signin form

--- src/assets/components/auth/SignInForm.tsx
+++ src/assets/components/auth/SignInForm.tsx
@@ -53,11 +53,11 @@ function SignInForm({onRevertForm}:any) {
         <motion.form
           initial={{x: 500}}
           animate={{x: 0}}
-          className="flex flex-col mt-10 w-4/12 m-auto gap-4 text-teal-800 bg-white p-12 rounded-lg"
+          className="flex flex-col mt-10 sm:w-1/2 xl:w-1/3 w-11/12 m-auto gap-4 text-teal-800 bg-white md:p-12 p-5 rounded-lg"
           onSubmit={handeSubmit}
         >
-          <h2 className="text-2xl font-bold">Sign in to Schedule</h2>
-          <p>Enter to your account</p>
+          <h2 className="text-sm sm:text-xl md:text-2xl font-bold">Sign in to Schedule</h2>
+          <p className="text-sm md:text-base">Enter to your account</p>
           <div className="flex flex-col">
             <label htmlFor="login">Username</label>
             <input ref={login} type="text" name="login" id="login" className="text-slate-900 border-2 border-black"  />
@@ -66,9 +66,9 @@ function SignInForm({onRevertForm}:any) {
             <label htmlFor="password">Password</label>
             <input ref={pass} type="password" name="password" id="password" className="text-slate-900 border-2 border-black"  />
           </div>
-          <div className="flex gap-5 m-auto">
+          <div className="flex sm:gap-5 gap-2 m-auto md:text-base sm:text-sm text-xs">
             <button type="button" onClick={onRevertForm}>Need to create an account?</button>
-            <button disabled={fetching} className="bg-cyan-600 rounded-lg w-40 border-2 border-black text-black h-10  disabled:bg-sky-300">
+            <button disabled={fetching} className="bg-cyan-600 rounded-lg sm:w-40 w-1/2 border-2 border-black text-black h-8 md:h-10 disabled:bg-sky-300">
               {fetching ? "Try to enter..." : "Sign in"}
             </button>
           </div>
