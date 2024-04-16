f6117d12f2c3025a108c77eac754d3d85787d0c2
Arthur <goolebyaqa@gmail.com>
2024-04-16 16:18:52 +0500

    feat: add responsive for sign in form

--- src/assets/components/auth/CreateUserForm.tsx
+++ src/assets/components/auth/CreateUserForm.tsx
@@ -70,11 +70,11 @@ function CreateUserForm() {
         <motion.form
           initial={{y: 500}}
           animate={{y: 0}}
-          className="flex flex-col mt-10 w-4/12 m-auto gap-4 text-teal-800 bg-white p-12 rounded-lg"
+          className="flex flex-col mt-10 sm:w-1/2 xl:w-1/3 w-11/12 m-auto gap-4 text-teal-800 bg-white md:p-12 p-5 rounded-lg"
           onSubmit={handeSubmit}
         >
-          <h2 className="text-2xl font-bold">Welcome to Schedule</h2>
-          <p>Registration of new User</p>
+          <h2 className="text-sm sm:text-xl md:text-2xl font-bold">Welcome to Schedule</h2>
+          <p className="text-sm md:text-base">Registration of new User</p>
           <div className="flex flex-col">
             <label htmlFor="login">Username</label>
             <input type="text" name="login" id="login" className="text-slate-900 border-2 border-black" />
@@ -83,8 +83,8 @@ function CreateUserForm() {
             <label htmlFor="password">Password</label>
             <input type="password" name="password" id="password" className="text-slate-900 border-2 border-black" />
           </div>
-          <div className="flex gap-5 m-auto">
-            <button disabled={fetching} className="bg-cyan-600 rounded-lg w-40 border-2 border-black text-black h-10 disabled:bg-sky-300">
+          <div className="flex sm:gap-5 gap-2 m-auto md:text-base sm:text-sm text-xs">
+            <button disabled={fetching} className="bg-cyan-600 rounded-lg sm:w-40 w-1/2 border-2 border-black text-black h-8 md:h-10 disabled:bg-sky-300">
               {fetching ? "Try to register..." : "Create User"}
             </button>
             <button type="button" onClick={handleShowSignIn}>Already have an account?</button>
